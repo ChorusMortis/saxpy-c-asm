891dac200917b7e91ab9b8064c0aeaeb0244cb45
Brett Harley Mider <brett_mider@dlsu.edu.ph>
2024-03-28 12:49:05 +0800

    Optimize saxpy Assembly code

--- saxpy.asm
+++ saxpy.asm
@@ -1,68 +1,39 @@
-section .data
-n dq 0
-Xptr dq 0
-Yptr dq 0
-Zptr dq 0
-tempfp dq 0 ; to transfer hex from register to xmm
-
 section .text
+bits 64
+default rel
 global saxpyAsm
 saxpyAsm:
-    ; set up frame
+    ; set up frame for > 4 parameters
     push rbp
     mov rbp, rsp
     add rbp, 16
 
-    ; initialize parameters
-    mov [n], rcx
+    ; rcx <- n
     ; xmm1 <- A
-    mov [Xptr], r8
-    mov [Yptr], r9
-    mov rax, [rbp+32]
-    mov [Zptr], rax
+    ; r8 <- X pointer
+    ; r9 <- Y pointer
+    ; rbp+32 <- Z pointer
 
-    ; initialize idx
-    mov rcx, 0
+    ; rdx <- offset for indexing
+    xor rdx, rdx
+
+    ; rax <- Z pointer
+    mov rax, [rbp+32]
 
     start:
-    ; while idx < ctr, continue
-    cmp rcx, [n]
-    jz end
-
-    ; get &X[i], dereference, and move to variable
-    mov rax, [Xptr]
-    mov rax, [rax]
-    mov [tempfp], rax
-
-    ; move X[i] to xmm0
-    movss xmm0, [tempfp]
-
-    ; multiply X[i] (xmm0) to A (xmm1)
+    ; get X[i]
+    movss xmm0, [r8 + rdx]
+    ; multiply X[i] to A
     mulss xmm0, xmm1
+    ; get Y[i]
+    movss xmm2, [r9 + rdx]
+    ; add A*X[i] to Y[i]
+    addss xmm0, xmm2
+    ; write sum to Z[i]
+    movd [rax], xmm0
+    add rdx, 4
+    add rax, 4
+    loop start
 
-    ; get &Y[i] and dereference, and move to variable
-    mov rax, [Yptr]
-    mov rax, [rax]
-    mov [tempfp], rax
-
-    ; add Y[i] to A * X[i] (xmm0)
-    addss xmm0, [tempfp]
-
-    ; get &Z[i]
-    mov rax, [Zptr]
-
-    ; move function result (xmm0) to where Z[i] is pointing
-    movss [tempfp], xmm0
-    mov rdx, [tempfp]
-    mov dword [rax], edx
-
-    ; increment idx, pointers, then loop
-    inc rcx
-    add qword [Xptr], 4
-    add qword [Yptr], 4
-    add qword [Zptr], 4
-    jmp start
-
-    end:
     pop rbp
     ret
